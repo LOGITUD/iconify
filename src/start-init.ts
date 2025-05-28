51a0629c4248ba75e545895dd292d07fdaa93eca
Maxime Princelle <maxime@princelle.org>
2025-05-28 17:09:25 +0200

    ci: supprimer le fichier de licence, le fichier .gitkeep dans le répertoire icons, et ajuster le script d'initialisation pour lister les fichiers dans le répertoire icons

--- src/start-init.ts
+++ src/start-init.ts
@@ -2,11 +2,19 @@ import { config } from 'dotenv';
 import { loaded } from './data/loading.js';
 import { loadEnvConfig } from './misc/load-config.js';
 import { initAPI } from './init.js';
+import path from 'path';
+import { readdir } from 'fs/promises';
 
 (async () => {
 	// Configure environment
 	config();
 	loadEnvConfig();
+
+	console.log('Starting API initialization...');
+
+	// List files in icons directory
+	const files = await readdir(path.join(__dirname, 'icons'));
+	console.log(files);
 
 	// Init API
 	await initAPI({
